a42a3f11caf57d286c3a645a1a5017c6f09e8c99
Udayakumar, Anandhu <audayakumar@deloitte.com>
2022-03-18 22:12:08 +0530

    inital commit

---
+++ src/main/java/com/au/hello/world/AuHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.au.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/au/hello/world/AuHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.au.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
